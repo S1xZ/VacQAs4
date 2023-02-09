3f2ef041ce0e4eab0c9bf2259214ff35af8f9128
S1xZ <punya.gnwdn@gmail.com>
2023-02-09 22:16:08 +0700

    Assign6

--- server.js
+++ server.js
@@ -1,37 +1,34 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 // Load env vars
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
 
 const app = express();
 
-// app.get('/api/v1/hospitals', (req, res) => {
-//     //1. res.send('<h1>Hello from Express</h1>');
-//     //2. res.send({ name: 'Brad' });
-//     //3. res.json({ name: 'Brad' });
-//     //4. res.sendStatus(400);
-//     //5. res.status(400).json({ success: false });
-//     res.status(200).json({ success: true, msg: 'Show all hospitals' });
-// });
+// Body parser
+app.use(express.json());
+const hospitals = require("./routes/hospitals");
 
-// app.get('/api/v1/hospitals/:id', (req, res) => {
-//     res.status(200).json({ success: true, msg: `Show hospital ${req.params.id}` });
-// });
-
-// app.post('/api/v1/hospitals', (req, res) => {
-//     res.status(200).json({ success: true, msg: 'Create new hospital' });
-// });
-
-// app.put('/api/v1/hospitals/:id', (req, res) => {
-//     res.status(200).json({ success: true, msg: `Update hospital ${req.params.id}` });
-// });
-
-// app.delete('/api/v1/hospitals/:id', (req, res) => {
-//     res.status(200).json({ success: true, msg: `Delete hospital ${req.params.id}` });
-// });
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, data: { id: 1 } });
+});
 
 app.use("/api/v1/hospitals", hospitals);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
