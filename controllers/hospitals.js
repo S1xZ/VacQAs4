af5ceef43a521a280b3421409e0e4525b29e1748
S1xZ <punya.gnwdn@gmail.com>
2023-03-16 22:23:48 +0700

    // WIP

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -5,10 +5,73 @@ const Hospital = require("../models/Hospital");
 //@access   Public
 exports.getHospitals = async (req, res, next) => {
   try {
-    const hospitals = await Hospital.find();
+    let query;
+
+    //Copy req.query
+    const reqQuery = { ...req.query };
+
+    //Fields to exclude
+    const removeFields = ["select", "sort", "page", "limit"];
+
+    //Loop over removed fields and delete them from reqQuery
+    removeFields.forEach((param) => delete reqQuery[param]);
+    console.log(reqQuery);
+    //Create query string
+    let queryStr = JSON.stringify(req.query);
+    queryStr = queryStr.replace(
+      /\b(gt|gte|lt|lte|in)\b/g,
+      (match) => `$${match}`
+    );
+
+    query = Hospital.find(JSON.parse(queryStr));
+
+    // Select Fields
+    if (req.query.select) {
+      const fields = req.query.select.split(",").join(" ");
+      query = query.select(fields);
+    }
+
+    // // //Sort
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    //Pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 25;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await Hospital.countDocuments();
+
+    // console.log(startIndex);
+    query = query.skip(12).limit(3);
+
+    console.log(query);
+    const hospitals = await query;
+
+    // Pagination result
+    const pagination = {};
+    console.log(pagination);
+    if (endIndex < total) {
+      pagination.next = {
+        page: page + 1,
+        limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      pagination.prev = {
+        page: page - 1,
+        limit,
+      };
+    }
     res.status(200).json({
       success: true,
       count: hospitals.length,
+      pagination,
       data: hospitals,
     });
   } catch (err) {
--- server.js
+++ server.js
@@ -15,11 +15,14 @@ app.use(express.json());
 
 // Cookie parser
 
+// Router files
 const hospitals = require("./routes/hospitals");
 const auth = require("./routes/auth");
+const appointments = require("./routes/appointments");
 
 app.use("/api/v1/hospitals", hospitals);
 app.use("/api/v1/auth", auth);
+app.use("/api/v1/appointments", appointments);
 
 app.get("/", (req, res) => {
   res.status(200).json({ success: true, data: { id: 1 } });
